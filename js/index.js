06a8302f75c049174e66a211607e7ff270238ff3
Abraham E. Hanson <ekenehanson@gmail.com>
2024-04-29 15:06:27 +0100

    Can now send signUp email

--- js/index.js
+++ js/index.js
@@ -1,4 +1,23 @@
 
 
 
-console.log("You clicked on sign Up")
+document.getElementById("signUpBtn").addEventListener("click", function() {
+    let email = document.getElementById("signUpEmail").value;
+    fetch('https://new-arts-api.onrender.com/user/send-registration-email/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrf_token, // Make sure to include the CSRF token if CSRF protection is enabled
+        },
+        body: JSON.stringify({ email: email }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.message);
+        // You can display a success message or perform other actions here
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        // Handle errors here
+    });
+});
