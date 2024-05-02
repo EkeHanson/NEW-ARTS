de9866fbfbac1078422901a7269bef2f140c2921
Abraham E. Hanson <ekenehanson@gmail.com>
2024-05-02 23:29:55 +0100

    Can now create Category and Instructors

--- js/index.js
+++ js/index.js
@@ -3,32 +3,33 @@ import { SERVER_URL } from '../config.js';
 let loginPage = "https://new-arts-website.vercel.app/login.html"
 let coursesPage = "https://new-arts-website.vercel.app/courses.html"
 
-if (document.querySelector("#signUpBtn")){
-    let emailVerification = "https://new-arts-website.vercel.app/email-verification.html"
+// if (document.querySelector("#signUpBtn")){
+//     let emailVerification = "https://new-arts-website.vercel.app/email-verification.html"
 
-document.getElementById("signUpBtn").addEventListener("click", function() {
-    let email = document.getElementById("signUpEmail").value;
-    fetch('https://new-arts-api.onrender.com/user/send-registration-email/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            // 'X-CSRFToken': csrf_token, // Make sure to include the CSRF token if CSRF protection is enabled
-        },
-        body: JSON.stringify({ email: email }),
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.message);
-        if (data.message === 'Email sent successfully') {
-            window.location.href = emailVerification;
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        // Handle errors here
-    });
-});
-}
+// document.getElementById("signUpBtn").addEventListener("click", function() {
+//     alert('SIGN UP CLICKED')
+//     let email = document.getElementById("signUpEmail").value;
+//     fetch('https://new-arts-api.onrender.com/user/send-registration-email/', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             // 'X-CSRFToken': csrf_token, // Make sure to include the CSRF token if CSRF protection is enabled
+//         },
+//         body: JSON.stringify({ email: email }),
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data.message);
+//         if (data.message === 'Email sent successfully') {
+//             window.location.href = emailVerification;
+//         }
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
+//         // Handle errors here
+//     });
+// });
+// }
 
 if (document.querySelector("#signUpBtn")) {
     document.getElementById('signUpBtn').addEventListener('click', function(event) {
